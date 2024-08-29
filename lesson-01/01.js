eb2003d211d3e6c400100e98039879907856b001
Dmitriy Akula <dmitry.akula1985@gmail.com>
2024-08-29 11:03:47 +0300

    first task

---
+++ lesson-01/01.js
@@ -0,0 +1,12 @@
+// С помощью цикла while найдите сумму чисел от 1 до 20
+// Результат присвойте переменной sum
+
+// your code
+let sum =0
+let counter = 20
+while (counter >= 0) {
+    sum =sum + counter;
+    counter --;
+}
+console.log(sum);
+
